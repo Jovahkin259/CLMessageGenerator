7340f4db56f24bf1b86adf03d9819d3d8fa77d7d
Joshua Astle <jovahkin259@gmail.com>
2020-12-29 01:36:24 +0000

    Final Functionality

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const fs = require('fs');
-const parse = require('csv-parse');
-const stringify = require('csv-stringify');
+const csvToJson = require("convert-csv-to-json");
 
+// Store our quotes as JSON in array
+let quotes = csvToJson.fieldDelimiter(',').getJsonFromCsv("quotes.csv");
+
+// Generate random index to find our quote
+const randInt = Math.floor(Math.random() * quotes.length);
+
+// Print out the quote in a readable format
+console.log(`${quotes[randInt].quote} \n - ${quotes[randInt].author}`);
